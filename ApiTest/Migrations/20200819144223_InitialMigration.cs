64b0ae64f9ea6fd07a01d0913922644e5750ea00
aldab2 <ab.barnawi@gmail.com>
2020-08-19 17:43:44 +0300

    re added migration

---
+++ ApiTest/Migrations/20200819144223_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiTest.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Trucks",
+                columns: table => new
+                {
+                    TruckId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(nullable: false),
+                    PlateInfo = table.Column<string>(nullable: false),
+                    Brand = table.Column<string>(nullable: false),
+                    ModelYear = table.Column<string>(nullable: false),
+                    Condition = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trucks", x => x.TruckId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trucks");
+        }
+    }
+}
